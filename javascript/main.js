3843ef4b28a84634964daade13eceb71fdc5e7cb
jsmojnu <learnwithmojnu@gmail.com>
2021-12-10 16:55:00 +0600

    Check Leap Year or Not

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+document.querySelector('.checkBtn').addEventListener('click', function(){
+    const year = document.getElementById('year').value;
+    const result = isLeapYear(+year);
+    checkResult(result, year);
+    clearInput();
+})
+
+document.querySelector('#year').addEventListener('keyup', function(event){
+    const year = document.getElementById('year').value;
+    if(event.which === 13){
+       const result = isLeapYear(+year);
+       checkResult(result, year);
+       clearInput();
+    }
+})
+
+function checkResult(result, year){
+    if(result === true){
+        document.querySelector('.showResult').innerHTML = `${year} is a leap year`;
+    }
+    else{
+        document.querySelector('.showResult').innerHTML = `${year} is not a leap year`;
+    }
+}
+function clearInput(){
+    document.getElementById('year').value = "";
+}
+const isLeapYear = (year) =>  year%100 === 0 ? year%400 === 0 : year%4 === 0;
